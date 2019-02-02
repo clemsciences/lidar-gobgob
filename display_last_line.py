703a78b469d6939ffbc984246cd6a954b83cd1ac
clemsciences <clemsciences@aol.com>
2019-02-02 19:21:59 +0100

    Affiche les n dernières lignes

--- display_last_line.py
+++ display_last_line.py
@@ -2,6 +2,7 @@
 
 import sys
 from csv import reader
+from queue import Queue
 from os.path import isdir
 from src.affichage import affichage, affichage_brut_cartesien_une_image, init_affichage_brut_cartesien
 
@@ -9,13 +10,16 @@ if not isdir("./Logs/"):
     print("No measure to read")
     sys.exit(0)
 
+n_last_lines = 10
+last_lines = []
 with open("Logs/RawData.logs", "r") as f:
     data_reader = reader(f, delimiter=" ")
-    line = ""
     for line in data_reader:
-        pass
+        if len(last_lines) >= n_last_lines:
+            last_lines.pop(0)
+        last_lines.append(line)
 
-if not line:
+if last_lines:
     print("The log file is void")
     sys.exit(0)
 
@@ -27,7 +31,7 @@ if affichage:
     print("Affichage init")
     ax, fig = init_affichage_brut_cartesien()
 
-measures = [measure for measure in line[1:] if ":" in measure]
+measures = [measure for line in last_lines for measure in line[1:] if ":" in measure]
 dico = {float(m.split(":")[0]): float(m.split(":")[0]) for measure in measures for m in measure.split(":")}
 
 # Affichage des points
